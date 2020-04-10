35b31d8714e89bab2c1bf503b2f384e98e37a2aa
Trey Shuldberg <trey@Treys-MBP.attlocal.net>
2020-04-10 11:01:47 -0700

    deploy

--- finchcollector/main_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Finch, Feeding
-
-# Register your models here.
-
-admin.site.register(Finch)
-admin.site.register(Feeding)
--- finchcollector/main_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Feeding
-
-class FeedingForm(ModelForm):
-    class Meta:
-        model = Feeding
-        fields = ['date', 'meal']
